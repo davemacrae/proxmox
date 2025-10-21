f0687f6d8a40d39796f3a0ceb4a91d3b551c65a1
Dave MacRae <dave@macrae.org.uk>
2025-10-21 16:25:00 +0100

    Cleaned up code

--- cloudinit/mvm.sh
+++ cloudinit/mvm.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-VMID=999
+VMID=${1:-999}
+RELEASE=${2:-noble}
 STORAGE=local-lvm
 USER=dave
-
-set -x
-RELEASE=plucky
+AUTHORIZED_KEYS=base_authorized_keys
 IMAGE="${RELEASE}-server-cloudimg-amd64.img"
 IMAGE_URL="https://cloud-images.ubuntu.com/${RELEASE}/current/${RELEASE}-server-cloudimg-amd64.img"
+
+set +x
 
 rm -f ${IMAGE}
 
@@ -23,6 +24,8 @@ qm create $VMID --name "ubuntu-${RELEASE}-template" --ostype l26 \
     --vga serial0 --serial0 socket  \
     --net0 virtio,bridge=vmbr0
 qm importdisk $VMID ${IMAGE} $STORAGE > /dev/null 2>&1
+rm -f ${IMAGE}
+
 qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
 qm set $VMID --boot order=virtio0
 qm set $VMID --scsi1 $STORAGE:cloudinit
@@ -30,8 +33,8 @@ qm set $VMID --scsi1 $STORAGE:cloudinit
 cp script.yml /var/lib/vz/snippets/ubuntu.yaml
 
 qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
-qm set $VMID --tags ubuntu-template,plucky,cloudinit
+qm set $VMID --tags ubuntu-template,${RELEASE},cloudinit
 qm set $VMID --ciuser $USER
-qm set $VMID --sshkeys ~/.ssh/authorized_keys
+qm set $VMID --sshkeys ${AUTHORIZED_KEYS}
 qm set $VMID --ipconfig0 ip=dhcp
 qm template $VMID
