485099a310318cd7a803d3569b42db5ed47fb06e
Dave MacRae <dave@macrae.org.uk>
2025-10-22 11:31:02 +0100

    Shell script to install ansible - redundant

---
+++ cloudinit/ansible.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install software-properties-common
+
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+#Install Ansible
+sudo apt install ansible -y
+
+#Validate Ansible:
+ansible --version
