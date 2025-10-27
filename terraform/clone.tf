36db8b53daf53cecbf27f4df84c6ecab6e610fd8
Dave MacRae <dave@macrae.org.uk>
2025-10-27 15:12:40 +0000

    Increased default mem size

--- terraform/clone.tf
+++ terraform/clone.tf
@@ -1,4 +1,6 @@
 resource "proxmox_virtual_environment_vm" "ubuntu_clone" {
+  # count = 1
+  # name      = "ubuntu-clone${count.index + 1}"
   name      = "ubuntu-clone"
   node_name = var.virtual_environment_node_name
 
@@ -7,22 +9,14 @@ resource "proxmox_virtual_environment_vm" "ubuntu_clone" {
   }
 
   agent {
-    # NOTE: The agent is installed and enabled as part of the cloud-init configuration in the template VM, see cloud-config.tf
-    # The working agent is *required* to retrieve the VM IP addresses.
-    # If you are using a different cloud-init configuration, or a different clone source
-    # that does not have the qemu-guest-agent installed, you may need to disable the `agent` below and remove the `vm_ipv4_address` output.
-    # See https://registry.terraform.io/providers/bpg/proxmox/latest/docs/resources/virtual_environment_vm#qemu-guest-agent for more details.
     enabled = true
   }
 
   memory {
-    dedicated = 768
+    dedicated = 2048
   }
 
   initialization {
-    dns {
-      servers = ["192.168.1.3"]
-    }
     ip_config {
       ipv4 {
         address = "dhcp"
@@ -32,5 +26,6 @@ resource "proxmox_virtual_environment_vm" "ubuntu_clone" {
 }
 
 output "vm_ipv4_address" {
+  # value = proxmox_virtual_environment_vm.ubuntu_clone[count.index].ipv4_addresses[1][0]
   value = proxmox_virtual_environment_vm.ubuntu_clone.ipv4_addresses[1][0]
 }
